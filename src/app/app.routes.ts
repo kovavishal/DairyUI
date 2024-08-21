abd9bccc7b153c3b670654521352231bae0236fc
kovavishal <kovavishal@gmail.com>
2024-08-21 22:24:10 +0530

    Initial Ui code commit

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { BillingComponent } from './modules/billing/billing.component';
-
-export const routes: Routes = [
-    { path: 'billing', component: BillingComponent },
-    { path: '', redirectTo: '/billing', pathMatch: 'full' },
-  ];
